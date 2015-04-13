c9255c7b00d1a4693199091ce46afc95b1eabae5
C0mplX <olemartin.skaug@gmail.com>
2015-04-13 13:47:36 +0200

    fixed blog issue

--- template/content-blogg-posts.php
+++ template/content-blogg-posts.php
@@ -5,7 +5,7 @@
 <div id="post-image-container">
 
 
-	<?php query_posts('category_name=blogg&offset=1');?>
+	<?php query_posts('category_name=blogg');?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	
@@ -14,7 +14,6 @@
 		
 
 		<?php the_post_thumbnail('single_post') ?>
-		<?php wp_reset_query(); ?>
 
 		<div class="post-image-category">
 			<?php echo get_the_category_list(); ?>
@@ -23,23 +22,25 @@
 	</div> <!-- #post-image -->
 
 	<div class="post-image-caption">
-
 		<?php the_post_thumbnail_caption(); ?>
-
 	</div> <!-- .post-image-caption -->
 
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	  <header class="entry-header">
+	  <a href="<?php echo get_the_permalink();?>">
 	    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	   </a>
 	  </header><!-- .entry-header -->
 
 	  <div class="entry-content">
 	    <?php the_content(); ?>
+	    <hr>
 	  </div><!-- .entry-content -->
-
+		
 	</article><!-- #post-## -->
+
 
 
 	
