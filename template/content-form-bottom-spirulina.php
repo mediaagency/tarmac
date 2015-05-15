35499541b46867b62a8b72803936f8e259ff6087
rmKristiansen <post@roykristiansen.com>
2015-05-15 16:06:43 +0200

    Spiralling form bottom is dynamic

--- template/content-form-bottom-spirulina.php
+++ template/content-form-bottom-spirulina.php
@@ -23,15 +23,32 @@
 		<div id='tab1'>
 			<div class="tab-content">
 				<div class="fp-form-container-left fp-form-container-force-bottom">
-					<div class="fp-form-sign-up-container form-min-height">
+					<div class="fp-form-sign-up-container">
 						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('spirulina') ) : 
 						endif; ?>
+						<div class="disclaimer-container">
+							<p class="disclaimer-info">
+								<?php echo get_theme_mod( 'theme_custom_form_disclaimer_2' ); ?>
+								<a href="<?php echo get_theme_mod( 'theme_custom_form_disclaimer_link_2' ); ?>">
+									<?php echo get_theme_mod( 'theme_custom_form_disclaimer_link_text_2' ); ?>
+								</a>		
+							</p>
+						</div> <!-- .disclaimer-container -->
 					</div>
 				</div>
 				<div class="fp-form-container-right fp-form-container-force-top">
 					<div class="fp-form-product form-min-height" style="background-image: url(' <?php echo get_stylesheet_directory_uri() ?>/img/product_spirulina.jpg');">
 						<div class="fp-form-product-text">
-							JA TAKK! Jeg vil prøve <br>90 dager med Spirulina <br>for kun kr <?php echo get_theme_mod( 'price_spirulina' ); ?>!
+							<span class="form-argument argument-1">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text1_2' ); ?>
+							</span>
+							<span class="form-argument argument-2">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text2_2' ); ?>
+							</span>
+							<span class="form-argument argument-3">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text3_2' ); ?>
+								<?php echo get_theme_mod( 'price_chlorella' ); ?>!
+							</span>
 						</div>	  				
 					</div>
 				</div>
@@ -44,14 +61,31 @@
 				<div class="fp-form-container-left">
 					<div class="fp-form-product form-min-height" style="background-image: url(' <?php echo get_stylesheet_directory_uri() ?>/img/product_chlorella.jpg');">
 						<div class="fp-form-product-text">
-							JA TAKK! Jeg vil prøve <br>90 dager med Chlorella <br>for kun kr <?php echo get_theme_mod( 'price_chlorella' ); ?>!
+							<span class="form-argument argument-1">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text1_1' ); ?>
+							</span>
+							<span class="form-argument argument-2">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text2_1' ); ?>
+							</span>
+							<span class="form-argument argument-3">
+								<?php echo get_theme_mod( 'theme_custom_form_selling_text3_1' ); ?>
+								<?php echo get_theme_mod( 'price_chlorella' ); ?>!
+							</span>
 						</div>	  				
 					</div>
 				</div>
 				<div class="fp-form-container-right">
-					<div class="fp-form-sign-up-container form-min-height">
+					<div class="fp-form-sign-up-container">
 						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('chlorella') ) : 
 						endif; ?>
+						<div class="disclaimer-container">
+							<p class="disclaimer-info">
+								<?php echo get_theme_mod( 'theme_custom_form_disclaimer_1' ); ?>
+								<a href="<?php echo get_theme_mod( 'theme_custom_form_disclaimer_link_1' ); ?>">
+									<?php echo get_theme_mod( 'theme_custom_form_disclaimer_link_text_1' ); ?>
+								</a>		
+							</p>
+						</div> <!-- .disclaimer-container -->
 					</div>
 				</div>
 			</div>
