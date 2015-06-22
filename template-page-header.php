8fc8638a8dd3f0183386f0e8d5943a6b2037a417
C0mplX <olemartin.skaug@gmail.com>
2015-06-22 13:34:08 +0200

    Added some styling

--- template-page-header.php
+++ template-page-header.php
@@ -1,12 +1,16 @@
 
 
 
-<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID )); // Get bg-image url ?> 
+<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID )); // Get bg-image url ?>
 <div id="page-header" class="fluid-container" style="background-image: url('<?php echo $url; ?>');">
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-		<h1><?php the_title(); ?></h1>
+		<?php 
+			if( esc_attr( @get_post_meta( $post->ID, 'ma_ta_section_header_onoff', true ) == "ON" ) ) {
+				$small_padding = 'small_padding'; 
+			}
+		?> 
+		<h1 class="<?php echo $small_padding; ?>"><?php the_title(); ?></h1>
 		<!--Checks if the on/off button is on and display information accordingly.-->
 		<?php if( esc_attr( @get_post_meta( $post->ID, 'ma_ta_section_header_onoff', true ) == 'ON' ) ):  ?>
 			<div class="hero-image-sub-heading">
