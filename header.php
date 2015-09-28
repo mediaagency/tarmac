5c933019260d0adcdebc629ad9b3957335a3bbd8
C0mplX <olemartin.saug@gmail.com>
2015-09-28 16:00:54 +0200

    added analytics

--- header.php
+++ header.php
@@ -50,13 +50,27 @@
   js.src = "//connect.facebook.net/nb_NO/sdk.js#xfbml=1&version=v2.3";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<!-- Facebook End  -->    
+<!-- Facebook End  -->  
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1049730585067627');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1049730585067627&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+  
 
 
 <!-- Google-kode for remarketing-taggen -->
-<!--------------------------------------------------
-Remarketing-tagger kan ikke knyttes til personlig identifiserende informasjon eller legges inn på sider som er relatert til sensitive kategorier. Du finner mer informasjon og flere instruksjoner for hvordan du konfigurerer taggen, på: http://google.com/ads/remarketingsetup
---------------------------------------------------->
+<!--Remarketing-tagger kan ikke knyttes til personlig identifiserende informasjon eller legges inn på sider som er relatert til sensitive kategorier. Du finner mer informasjon og flere instruksjoner for hvordan du konfigurerer taggen, på: http://google.com/ads/remarketingsetup-->
 
 <script type="text/javascript">
 /* <![CDATA[ */
