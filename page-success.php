c49de92ed16c1142075e2ff46a6486f1326febd8
C0mplX <olemartin.saug@gmail.com>
2015-10-05 15:47:07 +0200

    Added conversion

--- page-success.php
+++ page-success.php
@@ -58,6 +58,23 @@ if( isset($_COOKIE['orderID'] ) ) {
 }
 ?>
 
+<!-- Facebook Conversion Code for Registreringer - Superliv 1 -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6033864543547', {'value':'0.00','currency':'NOK'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6033864543547&amp;cd[value]=0.00&amp;cd[currency]=NOK&amp;noscript=1" /></noscript>
+
 <!-- Google Code for Superliv nettbutikk Conversion Page -->
 <script type="text/javascript">
 /* <![CDATA[ */
