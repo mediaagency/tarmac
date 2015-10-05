04f461fea691d00576090d4155bf8bbace7b589c
C0mplX <olemartin.saug@gmail.com>
2015-10-05 15:00:31 +0200

    Added new facebook pixel

--- header.php
+++ header.php
@@ -60,12 +60,13 @@ n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
 document,'script','//connect.facebook.net/en_US/fbevents.js');
 
-fbq('init', '1049730585067627');
+fbq('init', '951538451586779');
 fbq('track', "PageView");</script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1049730585067627&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=951538451586779&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+
   
 
 
